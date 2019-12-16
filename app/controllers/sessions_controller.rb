9eb4b4100314cea00dbb6127d39fb573dc979345
yuki ueki <karoo4413@gmail.com>
2019-12-16 19:28:49 +0900

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+      # ログイン成功した場合
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+
+end
