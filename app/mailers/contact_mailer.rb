ddc6bf9c80a723a7960ebe5ec65e02eb0de1d81b
yuki ueki <karoo4413@gmail.com>
2019-12-22 11:07:39 +0900

    メール機能エラー修正2

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer 
   def contact_mail(feed)
     @feed = feed
-  mail to: "@feed.email", subject: "投稿確認メール"
+
+  mail to:@feed.user.email, subject: "投稿確認メール"
   end
 end
